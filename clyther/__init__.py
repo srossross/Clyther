f3808c3d2caeed97ffab9d97fa1679ad02d3cb81
srossross <srossross@enthought.com>
2012-01-07 11:43:09 -0800

    ENH: Added test function to clyhter namespace.

--- clyther/__init__.py
+++ clyther/__init__.py
@@ -10,3 +10,16 @@ from __future__ import print_function
 from cly_kernel import kernel, global_work_size, local_work_size, developer, task, cache
 
 from clyther.queue_record import QueueRecord
+import sys
+
+def test(stream=sys.stdout, descriptions=True, verbosity=2, failfast=False, buffer=False):
+    '''
+    Load and run the clyther test suite.
+    '''
+    import unittest as _unit
+    import os as _os
+    star_dir = _os.path.dirname(__file__)
+    test_suite = _unit.defaultTestLoader.discover(star_dir)
+    runner = _unit.TextTestRunner(stream, descriptions, verbosity, failfast, buffer)
+    runner.run(test_suite)
+    
